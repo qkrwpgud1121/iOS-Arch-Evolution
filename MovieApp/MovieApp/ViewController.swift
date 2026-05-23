61f225e64e49888de6e3fe1df5fe451bfe90b183
박제형 <qkrwpgud1121@gmail.com>
2026-05-23 17:43:06 +0900

    feat: Add Phase 1 MovieApp MVC project structure

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by 박제형 on 5/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
